07344c24220388579941b94c1b0cd3a194fc4923
Subbu4382 <subrahmanyamdunne@gmail.com>
2025-11-06 17:26:24 +0530

    python file added

--- event_props/src/components/card.jsx
+++ event_props/src/components/card.jsx
@@ -9,16 +9,33 @@ export class Card extends Component {
           width: "15rem",
           borderRadius: "5px",
           border: "2px solid skyblue",
-          display:"flex",flexDirection:"column",
-          justifyContent:"center",
-          alignItems:"center",
-          padding:"20px",
-          
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "20px",
         }}
       >
         <img src={this.props.productData.image} width={150} height={150} />
-        <p style={{margin:0,padding:"20px",color:"gray"}}>{this.props.productData.title}</p>
-        <p style={{margin:0,color:"blue",fontSize:"20px"}}>{this.props.productData.price}</p>
+        <p style={{ margin: 0, padding: "20px", color: "gray" }}>
+          {this.props.productData.title}
+        </p>
+        <p style={{ margin: 0, color: "blue", fontSize: "20px" }}>
+          {this.props.productData.price}
+        </p>
+        <button
+          style={{
+            margin: "10px",
+            padding: "5px",
+            color: "blue",
+            backgroundColor: "lightblue",
+            border: "None",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Add To Cart
+        </button>
       </div>
     );
   }
